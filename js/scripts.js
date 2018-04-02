b8e9b05d081d231a4bca372df0336ef55a5a3b2c
Bobby Martin and Luke Stockburger <luke@>
2018-04-02 15:55:27 -0700

    Add styles

--- js/scripts.js
+++ js/scripts.js
@@ -11,10 +11,9 @@ $(document).ready(function() {
     var todoInput = $("#todoItem").val();
     var newItem = new Task(todoInput)
 
-    $("#todoList ul").prepend("<li class='item mt-2'>" + newItem.todoItem + "<button class='btn btn-danger remove float-right ml-2'>X</button></li>");
+    $("#todoList ul").prepend("<li class='item text-light'>" + newItem.todoItem + "<button class='btn btn-danger remove float-right ml-2'>X</button></li>");
     $("#form")[0].reset();
 
-    //clicking button removes all list items...
     $(".remove").click(function() {
       $(this).parent().remove();
     });
