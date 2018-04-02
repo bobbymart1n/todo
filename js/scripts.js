9ebace3dccf3995d5d638ef9e4f56c3606f5c2ac
Bobby Martin and Luke Stockburger <luke@>
2018-04-02 14:13:29 -0700

    add .reset to clear input, add more user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,19 @@ function Task (todo) {
 
 
 //user interface logic
-(document).ready(function() {
+$(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
     var todoInput = $("#todoItem").val();
+    var newItem = new Task(todoInput)
+
+    $("#todoList ul").prepend("<li class='item'>" + newItem.todoItem + "<button class='btn btn-danger remove'>X</button></li>");
+    $("#form")[0].reset();
+
+
+    $(".remove").click(function() {
+      $(".item").remove();
+
+    });
   });
 });
