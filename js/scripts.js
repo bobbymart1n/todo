0d43081418a13475c3ad72cdc747734d4c77f8dc
Bobby Martin and Luke Stockburger <luke@>
2018-04-02 14:50:31 -0700

    adjust todoList

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
     var todoInput = $("#todoItem").val();
     var newItem = new Task(todoInput)
 
-    $("#todoList ul").prepend("<li class='item mt-2'>" + newItem.todoItem + "<button class='btn btn-danger remove ml-2'>X</button></li>");
+    $("#todoList ul").prepend("<li class='item mt-2'>" + newItem.todoItem + "<button class='btn btn-danger remove float-right ml-2'>X</button></li>");
     $("#form")[0].reset();
 
     //clicking button removes all list items...
